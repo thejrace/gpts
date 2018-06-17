fb305c86e1a5e818e1cc2dbb5022b3c6e39e18ea
thejrace <ahmetkanbur@gmail.com>
2018-06-17 14:25:38 +0300

    added new table definitions

--- inc/datatables.php
+++ inc/datatables.php
@@ -12,5 +12,6 @@
 	define( "DBT_GPEMPLOYEETASKDEFINITIONSSTATUSUPDATES", "gp_employee_task_definitions_status_updates" );
 	define( "DBT_GPEMPLOYEEPERIODICTASKDEFINITIONS", "gp_employee_periodic_task_definitions" );
 	define( "DBT_GPAPITRIGGERS", "gp_api_triggers");
+	define( "DBT_GPAPITRIGGERSNOTCHECKED", "gp_api_triggers_not_checked");
 	define( "DBT_APIUSERS", "gp_api_users");
 	define( "DBT_APIUSERDEVICES", "gp_api_user_devices");
