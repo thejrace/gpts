e2b29d6160d1d453b556371a6e84772810542caa
thejrace <ahmetkanbur@gmail.com>
2018-06-17 14:36:09 +0300

    added add method

--- inc/class/GPApiUser.php
+++ inc/class/GPApiUser.php
@@ -9,8 +9,43 @@
 
     class GPApiUser extends GPDataCommon {
 
+        private $passwordHashOptions = array( 'cost' => 12 );
+
         public function __construct( $val = null ){
-            parent::__construct( DBT_APIUSERS, array( "id", "email", "nick" ), $val );
+            parent::__construct( DBT_APIUSERS, array( "id", "email" ), $val );
+
+            $this->dbFormKeys = array(
+                "email" => array(
+                    "label" 		=> "Eposta",
+                    "unique" => true,
+                    "validation" 	=> array( "req" => true, "email" => true )
+                ),
+                "password" => array(
+                    "label" 		=> "Şifre",
+                    "validation" 	=> array( "req" => true )
+                ),
+                "user_group" => array(
+                    "label" 		=> "Grup",
+                    "validation" 	=> array( "req" => true, "posnum" => true )
+                ),
+                "date_added" => array(
+                    "label" 		=> "Grup",
+                    "validation" 	=> array( "req" => true )
+                )
+            );
+
         }
+
+        /*
+         * - add method, additional to the parent's add method we hash the password
+         *
+         * */
+        public function add( $input ){
+            // overwrite input[password] with hashed version
+            $hash = password_hash( $input["password"], PASSWORD_BCRYPT, $this->passwordHashOptions );
+            $input["password"] = $hash;
+            parent::add( $input );
+        }
+
 
     }
