a24406c6b4a26b5fe3aa6031b1acdbeecd6eaada
thejrace <ahmetkanbur@gmail.com>
2018-06-17 13:36:37 +0300

    give an error when required input is not passed

--- inc/class/GPDataCommon.php
+++ inc/class/GPDataCommon.php
@@ -88,8 +88,13 @@
 			$updateKeys = array();
 			$updateVals = array();
 			foreach( $this->dbFormKeys as $key => $value ){
-				// if key is not within the input skip it
-				if( !isset( $input[$key] ) ) continue;
+                // if non required key is not within the input skip it
+                if( !isset( $input[$key] ) && !isset($value["validation"]["req"]) ) continue;
+                // if required input is not within $_POST
+                if( !isset($input[$key])){
+                    $this->returnText = $value["label"] . " verisi yok.";
+                    return;
+                }
 				// 1 - validation check
 				if( isset($value["validation"] ) ){
 					$Validation = new GPFormValidation;
