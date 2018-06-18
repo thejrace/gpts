78538b4bde26e23a68481d8620ef3d9ba013c7cf
thejrace <ahmetkanbur@gmail.com>
2018-06-18 12:17:21 +0300

    new tables

--- inc/datatables.php
+++ inc/datatables.php
@@ -5,6 +5,7 @@
 	define( "DBT_GPEMPLOYEEGROUPS", "gp_employee_groups" );
 	define( "DBT_GPEMPLOYEEDAILYPLANS", "gp_employee_daily_plans" );
 	define( "DBT_GPEMPLOYEEDAILYPLANSCHEMAS", "gp_employee_daily_plan_schemas" );
+	define( "DBT_GPEMPLOYEEDAILYPLANDEFINITIONS", "gp_employee_daily_plan_definitions" );
 	define( "DBT_GPTASKS", "gp_tasks" );
 	define( "DBT_GPTASKGROUPS", "gp_task_groups" );
 	define( "DBT_GPSUBTASKDEFINITIONS", "gp_sub_task_definitions" );
