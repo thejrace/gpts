4381bc5f2343646208b855d1635a0d7e5c45bbec
thejrace <ahmetkanbur@gmail.com>
2018-06-20 22:37:23 +0300

    new tables

--- inc/datatables.php
+++ inc/datatables.php
@@ -4,12 +4,14 @@
 	define( "DBT_GPEMPLOYEES", "gp_employees" );
 	define( "DBT_GPEMPLOYEEGROUPS", "gp_employee_groups" );
 	define( "DBT_GPEMPLOYEEDAILYPLANS", "gp_employee_daily_plans" );
+	define( "DBT_GPEMPLOYEEDAILYPLANSARCHIVE", "gp_employee_daily_plans_archive" );
 	define( "DBT_GPEMPLOYEEDAILYPLANSCHEMAS", "gp_employee_daily_plan_schemas" );
 	define( "DBT_GPEMPLOYEEDAILYPLANDEFINITIONS", "gp_employee_daily_plan_definitions" );
 	define( "DBT_GPTASKS", "gp_tasks" );
 	define( "DBT_GPTASKGROUPS", "gp_task_groups" );
 	define( "DBT_GPSUBTASKDEFINITIONS", "gp_sub_task_definitions" );
 	define( "DBT_GPEMPLOYEETASKDEFINITIONS", "gp_employee_task_definitions" );
+	define( "DBT_GPEMPLOYEETASKDEFINITIONSARCHIVE", "gp_employee_task_definitions_archive" );
 	define( "DBT_GPEMPLOYEETASKDEFINITIONSSTATUSUPDATES", "gp_employee_task_definitions_status_updates" );
 	define( "DBT_GPEMPLOYEEPERIODICTASKDEFINITIONS", "gp_employee_periodic_task_definitions" );
 	define( "DBT_GPAPITRIGGERS", "gp_api_triggers");
