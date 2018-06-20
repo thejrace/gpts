7cc3e5b33faf5614af6a9fe29616d9663d8fd412
thejrace <ahmetkanbur@gmail.com>
2018-06-20 22:13:34 +0300

    added status keys

--- inc/class/GPEmployeeDailyPlanDefinition.php
+++ inc/class/GPEmployeeDailyPlanDefinition.php
@@ -9,6 +9,11 @@
     */
     class GPEmployeeDailyPlanDefinition extends GPDataCommon {
 
+        // status definitons
+        public static $PAST = 0,   // past definitons
+                      $ACTIVE = 1, // current definiton
+                      $FUTURE = 3; // if employee's plan is changed, new daily definiton will be this one
+
         public function __construct( $val = null ){
             parent::__construct( DBT_GPEMPLOYEEDAILYPLANDEFINITIONS, array( "id" ), $val );
             $this->dbFormKeys = array(
