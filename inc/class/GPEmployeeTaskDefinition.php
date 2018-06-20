7ed73bbf0a2286a9d638678d3590dadec439984c
thejrace <ahmetkanbur@gmail.com>
2018-06-20 22:52:32 +0300

    set archiveTable name

--- inc/class/GPEmployeeTaskDefinition.php
+++ inc/class/GPEmployeeTaskDefinition.php
@@ -13,6 +13,7 @@
 	
 		public function __construct( $val = null, $archive = false ){
 		    if( isset($val) && isset($archive) && $archive ) $this->archiveFlag = true;
+            $this->archiveTable = DBT_GPEMPLOYEEDAILYPLANSARCHIVE;
 			parent::__construct( DBT_GPEMPLOYEETASKDEFINITIONS, array( "id" ), $val );
 			// unique groups should be on top to save time for unique checks
 			$this->dbFormKeys = array(
