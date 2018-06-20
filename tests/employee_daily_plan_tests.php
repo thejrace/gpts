6e44733c64e12a1d8c4afdcaef26665114fbe7d8
thejrace <ahmetkanbur@gmail.com>
2018-06-20 14:49:17 +0300

    init

--- tests/employee_daily_plan_tests.php
+++ tests/employee_daily_plan_tests.php
@@ -5,26 +5,29 @@
 
 	require CLASS_DIR . "GPFormValidation.php";
 	require CLASS_DIR . "GPDataCommon.php";
-	require CLASS_DIR . "GPEmployee.php";
+	require CLASS_DIR . "GPEmployeeDailyPlanDefinition.php";
 	require CLASS_DIR . "GPEmployeeDailyPlanSchema.php";
 	require CLASS_DIR . "GPEmployeeDailyPlan.php";
+    require CLASS_DIR . "GPEmployee.php";
 
 
 	function definePlan(){
 
 	    $Employee = new GPEmployee(1);
 	    $exInput = array(
-
-
-
+            "daily_plan_schema_id" => 1,
+            "daily_plan" => array(
+                array( "plan_order" => 1, "start" => "08:00", "end" => "09:00", "employee_id" => 1, "status" => "B", "active_date" => "2018-06-18" ),
+                array( "plan_order" => 2, "start" => "09:00", "end" => "10:00", "employee_id" => 1, "status" => "B", "active_date" => "2018-06-18" ),
+                array( "plan_order" => 3, "start" => "10:00", "end" => "11:00", "employee_id" => 1, "status" => "B", "active_date" => "2018-06-18" )
+            )
         );
-
-	    $Employee->definePlan( $PlanSchema );
+	    $Employee->definePlan( $exInput );
 
     }
 
 	function main(){
-
+        definePlan();
 
 
     }
