8c64bd23a073db009a676d0a16da0ef41927dcad
thejrace <ahmetkanbur@gmail.com>
2018-09-19 20:20:07 +0300

    update

--- sql/import.bat
+++ sql/import.bat
@@ -3,4 +3,4 @@ cd ..
 cd ..
 cd mysql
 cd bin
-mysqldump -uroot -p"" gitas_es < "C:/xampp/htdocs/gpts/sql/gitas_es_last.sql"
+mysql -uroot -p"" gitas_es < "C:/xampp/htdocs/gpts/sql/gitas_es_last.sql"
